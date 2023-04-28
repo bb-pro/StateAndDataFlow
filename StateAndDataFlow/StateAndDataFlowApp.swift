738fc69a7d0f17694b36c0cf18631df030fc16bf
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-04-28 09:54:59 +0500

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Bektemur Mamashayev on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
